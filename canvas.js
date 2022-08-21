9712d5fc3c45daec9fceabf11b21dbb09dbaf3e1
Abhinandan Bhuse <82033076+i-am-abhi747@users.noreply.github.com>
2022-08-21 22:16:57 +0530

    c1

--- canvas.js
+++ canvas.js
@@ -1,9 +1,83 @@
+// setting up canvas
 
 var canvas = document.createElement("CANVAS")
-canvas.style.height = window.innerHeight + "px"
-canvas.style.width  = window.innerWidth  + "px"
+canvas.style.height = window.innerHeight         + "px"
+canvas.style.width  = (window.innerWidth - 300)  + "px"
 
 document.body.style.margin   = "0"
 document.body.style.overflow = "hidden"
 
 document.body.append(canvas)
+
+var ctx = canvas.getContext("2d")
+
+
+// background
+
+ctx.fillStyle = "#000"
+ctx.rect(0, 0, canvas.width, canvas.height)
+
+
+// whiteboard setup
+
+var isDown = false;
+var X,Y;
+var pathlib = [] // all prv completed path librery
+var path    = [] // current path
+
+var linewidth   = 2
+var strokeStyle = "#fff"
+
+
+// mouse magic
+
+$(canvas).mousedown(
+    function(e) {
+
+        // start drawing 
+
+        isDown = true
+        ctx.beginPath()
+
+        X = e.pageX - canvas.offsetLeft;
+		Y = e.pageY - canvas.offsetTop;
+		ctx.moveTo(X, Y);
+
+        path = [[X,Y]]
+
+    }
+)
+
+$(canvas).mousemove(
+    function(e) {
+
+        if (isDown) {
+
+            // draw
+
+            X = e.pageX - canvas.offsetLeft;
+		    Y = e.pageY - canvas.offsetTop;
+
+            ctx.lineto(X, Y)
+            path.push([X, Y])
+
+            ctx.linewidth   = linewidth
+            ctx.strokeStyle = strokeStyle
+			ctx.stroke();
+
+        }
+    }
+)
+
+$(canvas).mouseup(
+    function(e) {
+
+        // stop drawing
+
+        isDown = false
+        ctx.closePath();
+        pathlib.push(path)
+        console.log(path)
+        
+    }
+)
