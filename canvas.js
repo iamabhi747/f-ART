af7292be89715c3c660fa208f386566abdb66da4
Abhinandan Bhuse <82033076+i-am-abhi747@users.noreply.github.com>
2022-08-21 21:52:57 +0530

    c1

--- canvas.js
+++ canvas.js
@@ -1,4 +1,7 @@
 
-camvas = document.createElement("CANVAS")
+var camvas = document.createElement("CANVAS")
 canvas.style.height = window.innerHeight + "px"
 canvas.style.width  = window.innerWidth  + "px"
+
+document.body.style.margin = "0"
+document.body.append(canvas)
