f365f003a73a66f010c1cce485d7b4c70a795055
Abhinandan Bhuse <82033076+i-am-abhi747@users.noreply.github.com>
2022-08-21 21:55:53 +0530

    c1

--- canvas.js
+++ canvas.js
@@ -1,7 +1,9 @@
 
-var camvas = document.createElement("CANVAS")
+var canvas = document.createElement("CANVAS")
 canvas.style.height = window.innerHeight + "px"
 canvas.style.width  = window.innerWidth  + "px"
 
-document.body.style.margin = "0"
+document.body.style.margin   = "0"
+document.body.style.overflow = "hidden"
+
 document.body.append(canvas)
