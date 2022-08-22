2ab60137718be9181881890652fb815d5ac85a8b
Abhinandan Bhuse <82033076+i-am-abhi747@users.noreply.github.com>
2022-08-22 09:24:12 +0530

    c1

--- workbench.js
+++ workbench.js
@@ -1,5 +1,8 @@
 workbenches = document.getElementById("workbench-container").children
 
+document.getElementById("workbench").style.marginLeft = (window.innerWidth - 300) + "px"
+document.getElementById("workbench").style.width      = "300px"
+
 function setWorkbench(name) {
     var cur = document.getElementById("workbench-" + name)
     if (cur == null) { return }
